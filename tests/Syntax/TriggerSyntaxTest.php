9c27f1fb2c09b8de6d5d80df729a0a89533f20b8
Johnny Mast <mastjohnny@gmail.com>
2021-07-14 23:17:22 +0200

    Added tests for missing brackets

--- tests/Syntax/TriggerSyntaxTest.php
+++ tests/Syntax/TriggerSyntaxTest.php
@@ -48,4 +48,104 @@ class TriggerSyntaxTest extends ResponseTest
 
         $this->assertEquals($expected, $actual);
     }
+
+    public function testUnmatchedOpenOrCloseParenthesisBrackets() {
+        $node = new Node("+ this is missing an opentag)", 0);
+
+        $expected = "Unmatched right parenthesis bracket ()";
+        $actual = $node->checkSyntax();
+
+        $this->assertEquals($expected, $actual);
+
+        $node = new Node("+ this is missing an (opentag", 0);
+
+        $expected = "Unmatched left parenthesis bracket ()";
+        $actual = $node->checkSyntax();
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testValidOpenOrCloseParenthesisBrackets() {
+        $node = new Node("+ this is missing an <opentag>", 0);
+
+        $expected = null;
+        $actual = $node->checkSyntax();
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testUnmatchedOpenOrCloseSquareBrackets() {
+        $node = new Node("+ this is missing an opentag]", 0);
+
+        $expected = "Unmatched right square bracket []";
+        $actual = $node->checkSyntax();
+
+        $this->assertEquals($expected, $actual);
+
+        $node = new Node("+ this is missing an [opentag", 0);
+
+        $expected = "Unmatched left square bracket []";
+        $actual = $node->checkSyntax();
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testValidOpenOrCloseSquareBrackets() {
+        $node = new Node("+ this is missing an [opentag]", 0);
+
+        $expected = null;
+        $actual = $node->checkSyntax();
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testUnmatchedOpenOrCloseCurlyBrackets() {
+        $node = new Node("+ this is missing an opentag}", 0);
+
+        $expected = "Unmatched right curly bracket {}";
+        $actual = $node->checkSyntax();
+
+        $this->assertEquals($expected, $actual);
+
+        $node = new Node("+ this is missing an {opentag", 0);
+
+        $expected = "Unmatched left curly bracket {}";
+        $actual = $node->checkSyntax();
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testValidOpenOrCloseCurlyBrackets() {
+        $node = new Node("+ this is missing an {opentag}", 0);
+
+        $expected = null;
+        $actual = $node->checkSyntax();
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testUnmatchedOpenOrCloseAngledBrackets() {
+        $node = new Node("+ this is missing an opentag>", 0);
+
+        $expected = "Unmatched right angled bracket <>";
+        $actual = $node->checkSyntax();
+
+        $this->assertEquals($expected, $actual);
+
+        $node = new Node("+ this is missing an <opentag", 0);
+
+        $expected = "Unmatched left angled bracket <>";
+        $actual = $node->checkSyntax();
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testValidOpenOrCloseAngledBrackets() {
+        $node = new Node("+ this is missing an <opentag>", 0);
+
+        $expected = null;
+        $actual = $node->checkSyntax();
+
+        $this->assertEquals($expected, $actual);
+    }
 }
